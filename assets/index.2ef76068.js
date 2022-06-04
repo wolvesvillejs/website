7394a60006bdcb088b7d733a0d24dc42b5587ca8
mainleau <45238635+mainleau@users.noreply.github.com>
2022-06-04 19:30:19 +0000

    Deploying to gh-pages from @ wolvesvillejs/website@810aa60b3c22c12a3e4d67dc0c9f02c9b6275d7a 🚀

--- assets/index.2ef76068.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./Spinner.3bef24b4.js";import{d as e,o as r,D as t}from"./index.61008cc4.js";import"./ReducedMotion.a2fdc192.js";const m=e({setup(a){return(s,c)=>(r(),t(o))}});export{m as default};
